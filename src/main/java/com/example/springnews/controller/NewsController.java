fa3846659789ea2bc2151c290acab9e498fbde76
leechung6311@gmail.com <leechung6311@gmail.com>
2026-05-13 15:38:58 +0900

    5/13

--- src/main/java/com/example/springnews/controller/NewsController.java
+++ src/main/java/com/example/springnews/controller/NewsController.java
@@ -3,7 +3,6 @@ package com.example.springnews.controller;
 import com.example.springnews.model.News;
 import com.example.springnews.repository.NewsRepository;
 import java.util.List;
-import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -42,6 +41,16 @@ public class NewsController {
     news.setCnt(news.getCnt()+1);
     newsRepository.save(news);
     return news;
+  }
+
+  @PostMapping("/news/update")
+  public String updateNews(News news){
+    News savedNews = newsRepository.findNewsById(news.getId());
+    savedNews.setTitle(news.getTitle());
+    savedNews.setWriter(news.getWriter());
+    savedNews.setContent(news.getContent());
+    newsRepository.save(savedNews);
+    return "redirect:/newsmain";
   }
 
   @GetMapping("/news/delete")
